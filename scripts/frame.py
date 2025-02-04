67bd630ce7fb943149b942d65381bc6d43ff3c32
alberto <albertoperez@agape.org.sv>
2025-02-04 15:51:54 -0600

    login, liquidations and CC's added

--- scripts/frame.py
+++ scripts/frame.py
@@ -3,17 +3,27 @@ from tkinter import ttk, messagebox, filedialog
 import threading
 import time
 from pdf import (
-    processPdf, renameFiles, getMessage, getErrorsCounter
+    processPdf, createTextFile, renameFiles, getMessage, getErrorsCounter, reset
 )
 
+TitleApp = "Proceso de Archivos"
+Input = None
 Directory = None
+Directory2 = None
 Window = None
+LoginWindow = None
 Label = None
+MessageLabel = \
+    "Presione \"Seleccionar Directorio\" para abrir una carpeta\n y despues presione \"Procesar\""
 StartTime = None
 TimeLabel = None
 Running = False
 DirButton = None
+Dir2Button = None
 ProcessButton = None
+CmbOptions = None
+CmbArray = ["Renombrar PDF's", "Procesar Liquidaciones", "Procesar Créditos Fiscales"]
+Option = 1
 
 def updateTimer():
     if not Running: return
@@ -31,51 +41,100 @@ def showAlert(message, type = 1):
 def cerrar():
     Window.destroy()
 
-def selectDirectory():
-    global Directory, Label
+def selectDirectory(option):    
+    global Directory, Directory2, Label    
     Window.update_idletasks()    
     directory = filedialog.askdirectory(parent=Window, title="Seleccionar Directorio")
     if not directory: return
-    Directory = directory
-    Label.config(text=f"{directory}")
+    if option == 1: Directory = directory
+    else: Directory2 = directory
+    Label.config(text=f"{MessageLabel}\n- {Directory}\n- {Directory2}")
 
-def process():    
+def processOptions(event):
+    global Option, Directory, Directory2
+    #Directory = Directory2 = ""
+    Option = 1 if CmbOptions.get() == CmbArray[0] else \
+        2 if CmbOptions.get() == CmbArray[1] else 3
+    Dir2Button.config(state= tk.DISABLED if Option == 1 else tk.ACTIVE)    
+
+def process():        
+    global MessageLabel
     if not Directory:
-        return showAlert('No ha seleccionado una carpeta', 2)
+        return showAlert(f'No ha seleccionado una carpeta ', 2)
+    if Option == 2 and not Directory2:
+        return showAlert(f'No ha seleccionado una carpeta para crear el archivo de texto', 2)
     if not messagebox.askyesno('Confirmación', '¿Esta seguro de procesar los archivos?'):
         return
-    Label.config(text=f"Procesando archivos en:\n \"{Directory}\"\n\nPor favor espere...")    
+    MessageLabel = f"Procesando archivos por favor espere. \n- {Directory}\n- {Directory2}"
+    Label.config(text=MessageLabel)    
     threading.Thread(target=runProcess, daemon=True).start()        
 
 def runProcess():
-    global StartTime, Running, ProcessButton, DirButton
+    global StartTime, Running, ProcessButton, DirButton, MessageLabel
     try:
         StartTime = time.time()
         Running = True
         ProcessButton.config(state=tk.DISABLED)
         DirButton.config(state=tk.DISABLED)
+        Dir2Button.config(state=tk.DISABLED)
         updateTimer()
-        processPdf(Directory)
-        renameFiles()
+        processPdf(Option, Directory)
+        if Option == 1: renameFiles()
+        else: createTextFile(Directory2)
         Running = False
         message = getMessage()
-        Label.config(text=f"¡Proceso completado! \n{message}")
+        MessageLabel = f"¡Proceso completado! \n{message}"
+        Label.config(text=MessageLabel)
         if getErrorsCounter() == 0:
-            return messagebox.showinfo('Confirmación', message)            
-        messagebox.showerror('Error', message)    
+            return messagebox.showinfo(f"{CmbOptions.get()}", message)            
+        messagebox.showerror(f"{CmbOptions.get()} Error", message)    
     except Exception as e:
         messagebox.showerror(f"Error principal: {str(e)}")
     finally:
         ProcessButton.config(state=tk.NORMAL)
         DirButton.config(state=tk.NORMAL)
+        if Option == 2: Dir2Button.config(state=tk.NORMAL)
+        reset()
+
+def validateUser():
+    text = Input.get()
+    if text == '' or text != "230498": 
+        Input.delete(0, tk.END)
+        return messagebox.showerror('Error', "Contraseña erronea")
+    LoginWindow.destroy()
+    frame()
+
+def login():
+    global Input, LoginWindow
+    # Crear LoginWindow
+    LoginWindow = tk.Tk()
+    LoginWindow.title(TitleApp)    
+    LoginWindow.resizable(False, False)
+    #LoginWindow.iconbitmap('icon.ico')
+    windowWidth = 300
+    windowHeigth = 200
+    screenWidth = LoginWindow.winfo_screenwidth()
+    screenHeight = LoginWindow.winfo_screenheight()
+    pos_x = (screenWidth // 2) - (windowWidth // 2)
+    pos_y = (screenHeight // 2) - (windowHeigth // 2)
+    LoginWindow.geometry(f"{windowWidth}x{windowHeigth}+{pos_x}+{pos_y}")
+    Input = ttk.Entry(LoginWindow, width=30, show="*")
+    Input.pack(pady=40)
+    style = ttk.Style()
+    style.configure("TButton", font=("Arial", 10), padding=5)    
+    style.configure("TEntry", font=("Arial", 10, "bold"), padding=5)    
+    button = ttk.Button(LoginWindow, text="Ingresar", command=validateUser)
+    button.pack(pady=0)    
+    LoginWindow.mainloop()
 
 def frame():    
-    global Label, Window, TimeLabel, DirButton, ProcessButton
+    global Label, Window, TimeLabel, DirButton, Dir2Button, ProcessButton, CmbOptions
     Window = tk.Tk()
-    Window.title("Renombrar Archivos")
+    Window.title(TitleApp)
+    #Window.iconbitmap('icon.ico')
     #Window.resizable(False, False)
     windowWidth = 500
-    windowHeigth = 300
+    windowHeigth = 400
     screenWidth = Window.winfo_screenwidth()
     screenHeight = Window.winfo_screenheight()
     pos_x = (screenWidth // 2) - (windowWidth // 2)
@@ -83,19 +142,33 @@ def frame():
     Window.geometry(f"{windowWidth}x{windowHeigth}+{pos_x}+{pos_y}")
     Label = tk.Label(
         Window, 
-        text="Presione \"Seleccionar Directorio\" para abrir una carpeta\n y despues presione \"Procesar\"", 
+        text=MessageLabel, 
         font=("Arial", 9)
     )
     TimeLabel = tk.Label(Window, text="Tiempo transcurrido: 00:00", font=("Arial", 10))
     TimeLabel.pack(pady=5)
     Label.pack(pady=20)
     style = ttk.Style()
-    style.configure("TButton", font=("Arial", 10), padding=5)
-    DirButton = ttk.Button(Window, text="Seleccionar Directorio", command=selectDirectory)
-    DirButton.pack(pady=10)
+    style.configure("TButton", font=("Arial", 10), padding=5)    
+    style.configure("TCombobox", font=("Arial", 10), padding=5)    
+    CmbOptions = ttk.Combobox(
+        Window, 
+        values=CmbArray, 
+        state="readonly"
+    )
+    CmbOptions.set(CmbArray[0])
+    CmbOptions.pack(pady=10)
+    CmbOptions.bind("<<ComboboxSelected>>", processOptions)
+    frame = tk.Frame(Window)
+    frame.pack(pady=10)
+    DirButton = ttk.Button(frame, text="Seleccionar Directorio", command=lambda:selectDirectory(1))
+    DirButton.grid(row=0, column=0, padx=10)
+    Dir2Button = ttk.Button(frame, text="Seleccionar Directorio", state=tk.DISABLED, command=lambda:selectDirectory(2))
+    Dir2Button.grid(row=0, column=1, padx=10)
     ProcessButton = ttk.Button(Window, text="Procesar", command=process)
     ProcessButton.pack(pady=10)
     Window.mainloop()
 
 if __name__ == "__main__":    
-    frame()
+    #frame()
+    login()
--- scripts/pdf.py
+++ scripts/pdf.py
@@ -1,16 +1,37 @@
 import pdfplumber
+from datetime import datetime
 import re
 import os
 
 #Código de Generación - Número de Control
-MatchText = "Número de Control"
-Directory = None
 ListFiles = []
+Strings = ""
+Directory = None
+BaseMatches = [
+    "Código de Generación", "Número de Control", "Sello de Recepción"
+]
+MatchTexts = []
+for i in BaseMatches: MatchTexts.append(i)
+# Liquidations
+Texts2 = [
+    "IVA de las operaciones a liquidar",
+    "Montos sujetos a percepción sin IVA",
+    "IVA percibido (2%)"
+]
+# CC's
+Texts3 = [
+    "Sumatoria de ventas", 
+    "Impuesto al Valor Agregado 13%",
+    "Total a Pagar"
+]
 ErrorsMessage = ""
-ErrorsNumber = 0
 Message = ""
+ErrorsNumber = 0
 FilesNumber = 0
+ProcessedNumber = 0
 RenamedNumber = 0
+#1 rename, 2 liquidations, 3 CC's
+ProcessOption = 1
 
 #type: 1 success, 2 error
 def setMessage(message, type = 1):
@@ -25,20 +46,66 @@ def getErrorsCounter():
     return ErrorsNumber
 
 def getMessage():
-    setMessage(f"Archivos PDF procesados: {len(ListFiles)}/{FilesNumber}")
-    setMessage(f"Archivos PDF renombrados: {RenamedNumber}/{FilesNumber}")
+    isFirstOption = ProcessOption == 1
+    setMessage(f"Archivos PDF procesados: {len(ListFiles) if isFirstOption else ProcessedNumber}/{FilesNumber}")
+    if isFirstOption: setMessage(f"Archivos PDF renombrados: {RenamedNumber}/{FilesNumber}")
     return f"{Message} \n \
         {'' if ErrorsNumber == 0 else \
         f"{ErrorsNumber} errores encontrados:\n{ErrorsMessage}"}"
 
+def reset():
+    global Message, ErrorsMessage, Strings, ErrorsNumber, \
+        FilesNumber, ProcessedNumber, RenamedNumber, ListFiles, MatchTexts 
+    Message = ErrorsMessage = Strings = ""    
+    ErrorsNumber = FilesNumber = ProcessedNumber = RenamedNumber = 0
+    ListFiles = []
+    MatchTexts = []
+    for i in BaseMatches: MatchTexts.append(i)    
+
+def spaces(text):    
+    if ProcessOption == 2:
+        if text == MatchTexts[0]:
+            return '               '
+        elif text == MatchTexts[1]:
+            return '                  '
+        elif text == MatchTexts[2]:
+            return '                 '
+        elif text == MatchTexts[3]:
+            return '  '
+        elif text == MatchTexts[4]:
+            return ''
+        elif text == MatchTexts[5]:
+            return '        	     '
+    else:
+        if text == MatchTexts[0]:
+            return ' '
+        elif text == MatchTexts[1]:
+            return '    '
+        elif text == MatchTexts[2]:
+            return '   '
+        elif text == MatchTexts[3]:
+            return '  '
+        elif text == MatchTexts[4]:
+            return '	       '
+        elif text == MatchTexts[5]:
+            return '	       '
+
 def findText(fileName):
+    string=""
+    dte="------------------------------------------------------------------------------\n\n"\
+        f"DTE: {"                                " if ProcessOption == 2 else "                  "}"
     try:
         with pdfplumber.open(os.path.join(Directory, fileName)) as pdf:
-            pageNumber = 0        
-            text = pdf.pages[pageNumber].extract_text()
-            result = re.search(rf"{MatchText}:\s*([^\s]+)", text)
-            if result: return result.group(1)
-        return None            
+            for page in pdf.pages:
+                for text in MatchTexts:                    
+                    result = re.search(rf"{re.escape(text)}:\s*([^\s]+)", page.extract_text())
+                    if result:
+                        if ProcessOption == 1: return result.group(1)
+                        if (text == MatchTexts[1]):
+                            dte+=f"{re.search(r'0+(\d+)$', result.group(1)).group(1)}"
+                        string+=f"{"IVA 13%" if ProcessOption == 3 and text == MatchTexts[4] else text}: {spaces(text)}{result.group(1)}\n"
+                        continue
+        return None if ProcessOption == 1 else f"{dte}\n{string}\n"
     except Exception as e:
         setMessage(f"error {fileName}: {str(e)}", 2)
     except FileNotFoundError as e:        
@@ -52,17 +119,27 @@ def findText(fileName):
     except ValueError as e:
         setMessage(f"error {fileName}: {str(e)}", 2)
 
-def processPdf(directory):
+def processPdf(option, directory):
     try:
-        global FilesNumber, Directory
+        global FilesNumber, Directory, ProcessOption, MatchTexts, Strings, ProcessedNumber
         Directory = directory
+        ProcessOption = option
+        isFirstOption = ProcessOption == 1
+        if(isFirstOption):
+            MatchTexts = [MatchTexts[1]]
+        else: MatchTexts.extend(Texts2 if ProcessOption == 2 else Texts3)
         for fileName in os.listdir(Directory): 
             FilesNumber += 1           
             try:
                 if not fileName.lower().endswith(".pdf"): continue
-                code = findText(fileName)
-                if code: ListFiles.append({"file": fileName, "code": code})
-                else: setMessage(f"No se pudo obtener el código del archivo: {fileName}", 2)
+                result = findText(fileName)
+                if not result or result == "": 
+                    setMessage(f"No se pudo obtener el resultado del archivo: {fileName}", 2)
+                if isFirstOption: 
+                    ListFiles.append({"file": fileName, "code": result})
+                    continue
+                ProcessedNumber += 1
+                Strings += result
             except Exception as e:
                 setMessage(f"error {fileName}: {str(e)}", 2)
     except Exception as e:
@@ -88,3 +165,11 @@ def renameFiles():
     except Exception as e:
         setMessage(f"error al renombrar los archivos: {str(e)}", 2)
     
+def createTextFile(directory):
+    date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    date = date.replace(' ', '_').replace(':', '_').replace('-', '_')
+    fileName = f"{'liquidaciones_' if ProcessOption == 2 else 'creditos_fiscales'}_{date}.txt"    
+    with open(os.path.join(directory, fileName), "a", encoding="utf-8") as file:
+        file.write(
+            f"{"LIQUIDACIONES" if ProcessOption == 2 else "CREDITOS FISCALES"} {ProcessedNumber}/{FilesNumber}\n\n{Strings}"
+        )    
